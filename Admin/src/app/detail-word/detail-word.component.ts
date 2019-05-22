b2eefac40b72c21d9bed246f1e85120942aacf64
15110081 <nhaminh.congnghephanmem@gmail.com>
2019-05-22 23:46:13 +0700

    22/05/2019

--- Admin/src/app/detail-word/detail-word.component.ts
+++ Admin/src/app/detail-word/detail-word.component.ts
@@ -5,7 +5,7 @@ import { Location } from '@angular/common';
 import { TokenStorageService } from '../auth/token-storage.service';
 import { Word } from 'src/model/word';
 import { Observable, Subscription, Subject } from 'rxjs';
-
+declare var $:any;
 @Component({
   selector: 'app-detail-word',
   templateUrl: './detail-word.component.html',
@@ -30,11 +30,43 @@ export class DetailWordComponent implements OnInit {
   {
       // this.wordService.file$.subscribe(file=>this.fileUpload=file);
    }
- 
+ JqueryDetailWord(){
+  $(document).ready(function () {
+    // Init Side nav
+    $('.button-collapse').sideNav();
+
+    // Init Slider
+    $('.slider').slider({
+      indicators: false,
+      height: 500,
+      transition: 500,
+      interval: 6000
+    });
+
+    // Autocomplete
+    $('.autocomplete').autocomplete({
+      data: {
+        "Aruba": null,
+        "Cancun Mexico": null,
+        "Hawaii": null,
+        "Florida": null,
+        "California": null,
+        "Jamaica": null,
+        "Europe": null,
+        "The Bahamas": null,
+      }
+    });
+
+    // Init Scrollspy
+    $('.scrollspy').scrollSpy();
+
+  });
+ }
   ngOnInit() {
-    this.getMovieFromRoute();
+    this.getWordFromRoute();
+    this.JqueryDetailWord();
   }
-  getMovieFromRoute(): void {
+  getWordFromRoute(): void {
     const id = +this.route.snapshot.paramMap.get('id');
     console.log(`this.route.snapshot.paramMap = ${JSON.stringify(this.route.snapshot.paramMap)}`);
     //Call service to "get movie from id" ?
@@ -60,7 +92,26 @@ export class DetailWordComponent implements OnInit {
   //  String(this.fileUpload);
      
   }
+  public messageImage: string;
+  selectedFilesImage: FileList;
+  preview(files, event) {
+    if (files.length === 0)
+      return;
 
+    var mimeType = files[0].type;
+    if (mimeType.match(/image\/*/) == null) {
+      this.messageImage = "Only images are supported.";
+      this.file = "";
+      return;
+    }
+    this.messageImage = "";
+    this.selectedFilesImage = event.target.files;
+    var reader = new FileReader();
+    reader.readAsDataURL(files[0]);
+    reader.onload = (_event) => {
+      this.file = reader.result;
+    }
+  }
   goBack(): void {
     this.location.back();
   }
